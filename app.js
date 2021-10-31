63681527d829fa19fdb91d795a4be5b3be0f16fd
saishanmukh591 <89558232+saishanmukh591@users.noreply.github.com>
2021-10-31 13:51:50 -0500

    add mods

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var partsRouter = require('./routes/parts');
+var addmodsRouter = require('./routes/addmods');
+
 
 
 var app = express();
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/parts', partsRouter);
+app.use('/addmods', addmodsRouter);
+
 
 
 // catch 404 and forward to error handler
